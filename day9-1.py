8810c4f44d4c10e4f9aec91674a1b4bd4614c1b8
Brian Zhu <brianzhu4321@gmail.com>
2022-12-11 15:46:28 -0500

    solved day9 part 1

--- day9-1.py
+++ day9-1.py
@@ -1,9 +1,10 @@
 visited = set()
+visited.add((0, 0))
 h = [0, 0]
 t = [0, 0]
 
 def isTouching(c1, c2):
-  if(c1[0] < c2[1]-1 or c1[0] > c2[0]+1 or c1[1] < c2[1]-1 or c2[1] > c2[1]+1):
+  if(c1[0] < c2[0]-1 or c1[0] > c2[0]+1 or c1[1] < c2[1]-1 or c1[1] > c2[1]+1):
     return False
   return True
 
@@ -12,18 +13,38 @@ while True:
   if(command == 'q'):
     break
   moves = int(command[2:])
-  direction = 1
+  hDirection = 1
+  vDirection = 1
   if(command[0] == 'L'):
-    direction = -1
-    moves *= -1
+    hDirection = -1
+  if(command[0] == 'U'):
+    vDirection = -1
 
   if(command[0] == 'R' or command[0] == 'L'):
-    for i in range(h[0], h[0]+moves, direction):
-      h[1] += direction
-      if(not isTouching(h, t)):
+    for i in range(0, moves):
+      h[1] += hDirection
+      if(isTouching(h, t) == False):
         if(t[0] == h[0]):
-          t[1] += direction
-          visited.add((t[0], t[1]))
-          
+          t[1] += hDirection
+        else:
+          if(h[0] > t[0]):
+            t[0] += 1
+          else:
+            t[0] -= 1
+          t[1] += hDirection
+        visited.add((t[0], t[1]))
+  elif(command[0] == 'U' or command[0] == 'D'):
+    for i in range(0, moves):
+      h[0] += vDirection
+      if(isTouching(h, t) == False):
+        if(t[1] == h[1]):
+          t[0] += vDirection
+        else:
+          if(h[1] > t[1]):
+            t[1] += 1
+          else:
+            t[1] -= 1
+          t[0] += vDirection
+      visited.add((t[0], t[1]))
 
-
+print(len(visited))
